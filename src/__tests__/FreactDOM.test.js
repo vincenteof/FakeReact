f593819f3a2da6a70ade4c8ab20d91bc4f054f2c
vincenteof <vincenteof@qq.com>
2019-02-08 15:02:01 +0800

    add some tests

--- src/__tests__/FreactDOM.test.js
+++ src/__tests__/FreactDOM.test.js
@@ -2,29 +2,30 @@ import { createElement, Component } from '../Freact'
 import { render } from '../FreactDOM'
 
 const e = createElement
-let root = document.createElement('div')
 
-describe('render', () => {    
-    test('has no children', () => {
-        expect(root.hasChildNodes()).toEqual(false)
+describe('render', () => {  
+    const root = document.createElement('div')
+    beforeAll(() => {
+        test('has no children', () => {
+            expect(root.hasChildNodes()).toEqual(false)
+        })
     })
 
-    // let effect = ''
-    // function sayHi() {
-    //     effect = 'Hi!!!'
-    // }
-    // class Button extends Component {
-    //     render() {
-    //         const clickHandler = this.props.onClick
-    //         const text = this.props.children
-    //         return e('button', { onclick: clickHandler }, text)
-    //     }
-    // }
+    function sayHi() {
+        console.log('Hi')
+    }
+    class Button extends Component {
+        render() {
+            const clickHandler = this.props.onClick
+            const text = this.props.children
+            return e('button', { onclick: clickHandler }, text)
+        }
+    }
 
-    // const btnElem = e(Button, { onClick: sayHi }, 'click')
-    // render(btnElem, root)
+    const btnElem = e(Button, { onClick: sayHi }, 'click')
+    render(btnElem, root)
 
-    // test('has created child node', () => {
-    //     expect(root.hasChildNodes()).toEqual(true)
-    // })
+    test('has created child node', () => {
+        expect(root.hasChildNodes()).toEqual(true)
+    })
 })
